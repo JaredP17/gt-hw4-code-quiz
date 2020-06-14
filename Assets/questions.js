c60d30bf012041ed781111f941d20b9ca52daa07
Jared Porter <jaredp17@me.com>
2020-06-14 07:51:29 -0500

    Created array of question objects

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,16 +1,16 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
+      question: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
     {
-      title: "The condition in an if/else statement is enclosed within ____.",
+      question: "The condition in an if/else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
     {
-      title: "Arrays in JavaScript can be used to store ____.",
+      question: "Arrays in JavaScript can be used to store ____.",
       choices: [
         "numbers and strings",
         "other arrays",
@@ -20,15 +20,15 @@ var questions = [
       answer: "all of the above"
     },
     {
-      title:
+      question:
         "String values must be enclosed within ____ when being assigned to variables.",
       choices: ["commas", "curly brackets", "quotes", "parentheses"],
       answer: "quotes"
     },
     {
-      title:
+      question:
         "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
       answer: "console.log"
     }
   ];
