d8b901c7574b60c2b32f4a93ef84ab2de979239f
Jared Porter <jaredp17@me.com>
2020-06-15 01:10:45 -0400

    Added html and js to display correctness of answers for a short period of time.

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,92 @@
-console.log("Hello World!");
+// VARIABLES
+var timer = document.querySelector("#timer");
+var start = document.querySelector("#start");
+var startBtn = document.querySelector("#start-btn");
+var questions = document.querySelector("#questions");
+var question = document.querySelector("#question");
+var choices = document.getElementById("answer-choices");
+var done = document.querySelector("#done");
+var timerInterval;
+var timeRemaining = 75; // Also sets the score
+var qIndex = 0;
+
+// FUNCTION DEFINITIONS
+
+// Countdown timer
+function setTimer() {
+  timerInterval = setInterval(function () {
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      alert("Out of time!");
+    } else {
+      timeRemaining--;
+      timer.textContent = timeRemaining;
+      console.log(timeRemaining);
+    }
+  }, 1000);
+}
+
+// Displays if answer was right/wrong for a short period
+function choiceResult(result) {
+  var result = document.querySelector("#choice-result");
+  result.classList.remove("d-none");
+  var time = 2;
+  var correctnessInterval = setInterval(function () {
+    if (time > 0) {
+      time--;
+    } else {
+      clearInterval(correctnessInterval);
+      result.classList.add("d-none");
+    }
+  }, 500);
+}
+
+function renderQuestions() {
+  questions.classList.remove("d-none");
+  question.textContent = questionsList[qIndex].question;
+  choices.innerHTML = "";
+
+  // Render answer buttons
+  for (var i = 0; i < questionsList[qIndex].choices.length; i++) {
+    var answerBtn = choices.appendChild(document.createElement("button"));
+    answerBtn.classList.add("btn", "btn-primary", "btn-sm", "btn-block");
+    answerBtn.textContent = questionsList[qIndex].choices[i];
+  }
+}
+
+// When the quiz is over
+function showScore() {
+  clearInterval(timerInterval);
+  questions.classList.add("d-none");
+  done.classList.remove("d-none");
+  document.getElementById("score").textContent = timeRemaining;
+}
+
+// FUNCTION CALLS
+
+// EVENT LISTENERS
+
+// Start Quiz
+startBtn.addEventListener("click", function () {
+  start.classList.add("d-none");
+  renderQuestions();
+  setTimer();
+});
+
+// Choose Answers
+choices.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (event.target.matches("button")) {
+    console.log("clicking " + event.target.textContent);
+    var choice = event.target.textContent;
+    qIndex++;
+    choiceResult("Correct!");
+    if (qIndex < questionsList.length && timeRemaining > 0) {
+      renderQuestions();
+    } else { // No more quiz questions
+      console.log("Out of questions!");
+      showScore();
+    }
+  }
+});
